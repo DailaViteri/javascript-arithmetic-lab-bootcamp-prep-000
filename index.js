402b46e0c5ecbace88a24e04b29af6113b84ec7f
Daila Viteri <dailaviteri@gmail.com>
2018-03-14 01:19:51 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,31 @@
+function add(a,b) {
+  return a+b
+}
+
+function subtract(a,b) {
+  return a-b
+}
+
+function multiply(a,b) {
+  return a*b
+}
+
+function divide(a,b) {
+  return a/b
+}
+
+function inc(a) {
+return a+1 }
+
+function dec(a) {
+return a-1 }
+
+ n = integer
+ function parseInt(n) {
+return n === n }
+  
+  
+
+describe('makeInt(n)', function() {
+  it('parses n as an integer and returns the parsed integer', function() {
+    expect(makeInt(a.toString())).toEqual(a)
